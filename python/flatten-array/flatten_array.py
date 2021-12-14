4548c5d41eb7439502b8be9e3707cf854db994d4
michael <23582917+m-01101101@users.noreply.github.com>
2021-12-14 23:43:14 +0000

    add some practice exercises

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,18 +1,14 @@
-from functools import reduce, map
 from typing import Iterable
 
 
-def flatten(iterable: Iterable) -> Iterable:
+def flatten(nested_list: Iterable) -> Iterable:
     """Take a nested list and return a single flattened list with all values except nil/null."""
+    return [element for element in flatten_list(nested_list)]
 
-    
-    while iterable:
-        _item = iterable.pop()
-        if type(_item) is not list:
-            output.insert(0, _item)
-        else:
-            output = output[::]
-            flatten(_item, output)
 
-    return output
-
+def flatten_list(nested_list: Iterable) -> Iterable:
+    for element in nested_list:
+        if isinstance(element, list):
+            yield from flatten_list(element)
+        elif element is not None and not isinstance(element, tuple):
+            yield element
